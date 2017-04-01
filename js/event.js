37d0a7ea35aa2b8e2252a4f1ff8aa17875afbcbe
John Kishore M <johnkishorem@gmail.com>
2017-04-01 22:31:38 +0900

    Added tab handling events

--- js/event.js
+++ js/event.js
@@ -1,5 +1,8 @@
-var notesBadgeString = { text: "0"};
-chrome.browserAction.setBadgeText(notesBadgeString);
+//Global Information storage
+var notesActiveTab = {"notesActiveTabId" : chrome.tabs.TAB_ID_NONE, 
+						"notesActiveTabMsgCount" : "0",
+						"notesActiveTabUrl" : ""
+					};
 
 //Context menu handling
 //createProperties for Context menu
@@ -8,12 +11,38 @@ var notesContextMenuProperties = { id : "notesContextMenu", title : "Add/Edit no
 //Add context menu to the browser
 chrome.contextMenus.create(notesContextMenuProperties);
 
-//Register an event listener for Context menu
-chrome.contextMenus.onClicked.addListener(notesContextMenuCallback);
-//End of Context menu handling
-
-//User event handler function - This function is called 
-//when the user clicks the ContextMenu of the extension
+//Calback function when the user clicks the ContextMenu of the extension
 function notesContextMenuCallback(notesContextMenuInfo, notesContextMenuTab) {
 	alert("hello");
 }
+//End of Context menu handling
+
+//Tab handling
+//Callbck function called when Active Tab is changed
+function notesActivatedTabCallback(notesActiveTabInfo) {
+	notesActiveTab.notesActiveTabId = notesActiveTabInfo.tabId;
+	chrome.tabs.get(notesActiveTab.notesActiveTabId, function(notesTabInfo) {
+		notesActiveTab.notesActiveTabUrl = notesTabInfo.url;
+	});
+	chrome.browserAction.setBadgeText({"text" : notesActiveTab.notesActiveTabMsgCount});
+}
+
+//Callback function called when a tab is updated
+function notesUpdatedTabCallback(notesTabId, notesTabChangeInfo, notesTabInfo) {
+	if( (notesTabId == notesActiveTab.notesActiveTabId) && (typeof notesTabChangeInfo.url != "undefined") ) {
+		notesActiveTab.notesActiveTabUrl = notesTabChangeInfo.url;
+	}
+}
+//End of Tab handling
+
+
+//Event listeners
+//Register an event listener for Context menu
+chrome.contextMenus.onClicked.addListener(notesContextMenuCallback);
+
+//Register an event listener for Active Tabs
+chrome.tabs.onActivated.addListener(notesActivatedTabCallback);
+
+//Register an event listener for Updated Tabs
+chrome.tabs.onUpdated.addListener(notesUpdatedTabCallback);
+//End of Event listeners
