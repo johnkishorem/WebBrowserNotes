3bc07c0bea73d44da50e07da4ee4d98fa62f8c45
John Kishore M <johnkishorem@gmail.com>
2017-04-01 17:25:30 +0900

    Added browser context menu of the extension

--- js/event.js
+++ js/event.js
@@ -1,2 +1,19 @@
-var notesBadgeText = { text: "0"}
-chrome.browserAction.setBadgeText(notesBadgeText)
+var notesBadgeString = { text: "0"};
+chrome.browserAction.setBadgeText(notesBadgeString);
+
+//Context menu handling
+//createProperties for Context menu
+var notesContextMenuProperties = { id : "notesContextMenu", title : "Add/Edit notes"};
+
+//Add context menu to the browser
+chrome.contextMenus.create(notesContextMenuProperties);
+
+//Register an event listener for Context menu
+chrome.contextMenus.onClicked.addListener(notesContextMenuCallback);
+//End of Context menu handling
+
+//User event handler function - This function is called 
+//when the user clicks the ContextMenu of the extension
+function notesContextMenuCallback(notesContextMenuInfo, notesContextMenuTab) {
+	alert("hello");
+}
