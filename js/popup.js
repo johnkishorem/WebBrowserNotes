b35a7e2e66a4fd4e7effb8b0b5e4d1546fbf9b4b
John Kishore M <johnkishorem@gmail.com>
2017-04-05 01:06:18 +0900

    Dynamic dom update for popup html based on a dev array

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,22 @@
+//Array of messages for dev purpose
+var notesDevArray = ["No notes to display", "No notes to display"];
+
+
+//Function to create an DOM with the provided message string
+//<div class='msgbox'><span class='deletebtn'>&times;</span>msgString</div>
+function notesCreateMsgBoxDom(msgString) {
+	return "<div class='msgbox'><span class='deletebtn'>&times;</span>"+ msgString + "</div>";
+}
+
 $(function(){
+
+	//Add notes to the popup window
+	for(var i =0; i < notesDevArray.length; i++) {
+		$('#notesMessages').append(notesCreateMsgBoxDom(notesDevArray[i]))
+	}	
+
+    //Add click listeners for delete button
 	$(".deletebtn").click(function() {
 		alert("Clicked");
-	});
+	});	
 });
